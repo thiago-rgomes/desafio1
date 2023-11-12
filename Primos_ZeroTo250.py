df205b48ef903fd5b998941a23a131f00742af47
ThiiagoGM <tgomes_2@hotmail.com>
2023-11-12 11:26:43 -0300

    print_primos_ok

---
+++ Primos_ZeroTo250.py
@@ -0,0 +1,12 @@
+
+#eeeee
+
+Primos = []
+
+for i in range(1, 251):
+    if i > 1:
+        for j in range(2, i):
+            if (i % j == 0):
+                break
+        else:
+            print(i)
